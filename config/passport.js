ae0954a674313cd46d36add5618c43005d2f8153
kristina-lim <kristinahlim@gmail.com>
2022-10-28 17:11:39 -0700

    fix bugs

--- config/passport.js
+++ config/passport.js
@@ -10,21 +10,21 @@ passport.use(new GoogleStrategy(
     },
     async function(accessToken, refreshToken, profile, cb) {
         try {
-            let user = await User.findOne({ googleID: profile.id });
-            if (user) return cb(null, user);
-            user = await User.create({
-                name: profile.displayName,
-                googleId: profile.id,
-                email: profile.emails[0].value,
-                avatar: profile.photos[0].value
-            });
-            return cb(null, user);
+          let user = await User.findOne({ googleId: profile.id });
+          if (user) return cb(null, user);
+          user = await User.create({
+            name: profile.displayName,
+            googleId: profile.id,
+            email: profile.emails[0].value,
+            avatar: profile.photos[0].value,
+          });
+          return cb(null, user);
         } catch (err) {
-            return cb(err);
+          return cb(err);
         }
-    }
-));
-
+      }
+    ));
+    
 passport.serializeUser(function(user, cb) {
     cb(null, user._id);
 });
