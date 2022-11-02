5a76e5825bf791cc1764066caa0d5c4719e9ef37
kristina-lim <kristinahlim@gmail.com>
2022-11-02 10:39:24 -0700

    bug fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var outfitsRouter = require('./routes/outfits');
 var commentsRouter = require('./routes/comments');
 
 var app = express();
-
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
